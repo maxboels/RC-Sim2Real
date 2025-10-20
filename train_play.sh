12d21e5d14032aff7bf50588683c6b1a224c1e42
maxboels <boelsmaxence@gmail.com>
2025-10-20 22:04:52 +0100

    add my-rc-car

--- train_play.sh
+++ train_play.sh
@@ -7,3 +7,10 @@ python isaac_lab/scripts/reinforcement_learning/skrl/train.py --task Isaac-Quant
 
 # playing
 python isaac_lab/scripts/reinforcement_learning/skrl/play.py --task Isaac-QuantumTracer-Direct-v0 --num_envs 32
+
+
+# bash version
+cd isaac_lab
+./isaaclab.sh -p source/standalone/workflows/skrl/train.py \
+    --task Isaac-Quantum-Tracer-Direct-v0 \
+    --num_envs 4096
