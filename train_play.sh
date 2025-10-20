dda9ae033fc3e62593bfe3de7ab2837479159024
maxboels <boelsmaxence@gmail.com>
2025-10-20 20:32:37 +0100

    add lb files

---
+++ train_play.sh
@@ -0,0 +1,9 @@
+
+#
+python isaac_lab/scripts/reinforcement_learning/skrl/train.py --task Isaac-QuantumTracer-Direct-v0 --num_envs 32
+
+# headless
+python isaac_lab/scripts/reinforcement_learning/skrl/train.py --task Isaac-QuantumTracer-Direct-v0 --num_envs 4096 --headless
+
+# playing
+python isaac_lab/scripts/reinforcement_learning/skrl/play.py --task Isaac-QuantumTracer-Direct-v0 --num_envs 32
